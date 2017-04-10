9dd32edd9444458026924889e8d12279449deb5c
eggee <timothy.eggenberger@adtran.com>
2017-04-10 12:53:11 -0500

    added a recursive loop to get each iTest file and write to a temp directory as text.

---
+++ itest_qc_teststep_extractor/itest_directory_qc_teststep_extractor_and_fileGen.py
@@ -0,0 +1,50 @@
+# procedure to read an iTest *.fftc test case and extract/display all the QualityCenter
+# test step text.
+import os
+__author__ = 'teggenbe'
+from lxml import etree, objectify
+
+parser = etree.XMLParser(remove_blank_text=True)
+path = "C:/iTest_4.0/CND_TA5000_BBDLC_Ethernet/BBDLC Ethernet/Voice/Bulk_Call_(Parsed)_Sip_MGCP_GR303/"
+# test_case = "n16s2-GR303-V2Combo-BulkCall-via-SM025Gx.fftc"
+file_listing = os.listdir(path)
+
+for each_file in file_listing:
+    file_to_parse = path + each_file
+    tree = etree.parse(file_to_parse)
+    #tree = etree.parse("025G1_Cross-Slot_LAG_2x10Gig_to_Cisco.fftc")
+    catalogue = tree.getroot()
+
+    filename = ('C:/temp/%s.txt' % each_file)
+    f = open(filename, 'a')
+    f.truncate()
+    for qualityCenterStepInfo in catalogue.iter("qualityCenterStepInfo"):
+        try:
+            stepname = qualityCenterStepInfo.get('stepName')
+        except AttributeError:
+            stepname = "BLANK"
+        try:
+            description = qualityCenterStepInfo.find("description").text
+        except AttributeError:
+            description = "BLANK"
+        try:
+            expectedResult = qualityCenterStepInfo.find("expectedResult").text
+        except AttributeError:
+            expectedResult = "BLANK"
+
+        print "Step Name:\n %s" % stepname
+        print "Description:\n %s" % description
+        print "Expected Result:\n %s \n" % expectedResult
+
+        # f.write(stepname + description + expectedResult)
+        f.write("\n")
+        f.write ("Step Name: \n" +  stepname + "\n")
+        f.write ("Description: \n" + description + "\n")
+        f.write ("Expected: \n" + expectedResult + "\n")
+        # f.write("Step Name:\n" %s "Description:\n" %s "Expected Result:\n \n" %s) % (stepname, description, expectedResult)
+
+f.close()
+
+
+
+
