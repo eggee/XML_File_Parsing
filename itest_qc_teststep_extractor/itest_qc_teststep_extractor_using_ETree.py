d8f00f49b3bbf81abbc13f5799b03c0ba814341e
eggee <timothy.eggenberger@adtran.com>
2017-04-10 09:05:35 -0500

    now uses full filepath

--- itest_qc_teststep_extractor/itest_qc_teststep_extractor_using_ETree.py
+++ itest_qc_teststep_extractor/itest_qc_teststep_extractor_using_ETree.py
@@ -1,12 +1,15 @@
 # procedure to read an iTest *.fftc test case and extract/display all the QualityCenter
 # test step text.
-
+import os
 __author__ = 'teggenbe'
 from lxml import etree, objectify
 
 parser = etree.XMLParser(remove_blank_text=True)
-path = "C:\iTest_4.0\CND_TA5000_BBDLC_Ethernet\BBDLC Ethernet\Voice\Bulk_Call_(Parsed)_Sip_MGCP_GR303"
-tree = etree.parse("n16s1-GR303-A2Combo-BulkCall-via-SM025Gx.fftc")
+path = "C:/iTest_4.0/CND_TA5000_BBDLC_Ethernet/BBDLC Ethernet/Voice/Bulk_Call_(Parsed)_Sip_MGCP_GR303/"
+test_case = "n16s2-GR303-V2Combo-BulkCall-via-SM025Gx.fftc"
+file_to_parse = path + test_case
+tree = etree.parse(file_to_parse)
+#tree = etree.parse("025G1_Cross-Slot_LAG_2x10Gig_to_Cisco.fftc")
 catalogue = tree.getroot()
 
 for qualityCenterStepInfo in catalogue.iter("qualityCenterStepInfo"):
